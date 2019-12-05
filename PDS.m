778066b0852b1ed2f56980c3c34cf48ab5243553
Antonio Azevedo <antonioazevedo.123@gmail.com>
2019-12-05 08:59:17 -0300

    Comentando o codigo

--- PDS.m
+++ PDS.m
@@ -2,32 +2,49 @@
 
 % Requisitos do projeto:
 %Frequencia de amostragem
+% ? = ?T = 2?f/fs = 2?/N
+% Para evitar aliasing a FA deve ser maior que a frequencia maxima do
+% sinal, dessa forma, a maior FA eh 2?.
 fsamp = 2*pi;
 
-% atenuacao minima >= 50db
-mags = [1 0];
-devs = [0.1 0.00315];
-
-% regiao de transicao delta < 0.1pi
+% regiao de transicao ?? < 0.1pi; usamos ?? = 0.08
 % frequencia de corte = pi/2 (a -6db)
-fcutsa = (pi/2)-(pi*0.04);
-fcutsb = (pi/2)+(pi*0.04);
+fcutsa = (pi/2) - (pi*0.04);
+fcutsb = (pi/2) + (pi*0.04);
 fcuts = [fcutsa fcutsb];
 
+% atenuacao minima >= 50db
+% A = ?20 log ?s => A = 50db
+% M = A?8 / 2,285?? => M = 73.134656563015362169508950346232?
+% B = 0.5842(A ? 21)^0.4 + 0.07886(A ? 21) => B = 4.5335141209812482327179764338239?
+% Oscilacao das bandas => ?s = 0.0031622776602 
+devs = [0.1 0.0031622776602];
+mags = [1 0];
+
 % Usando a janela de Kaiser
+% Nessa funcao obtemos os parametros para o Filtro de Kaiser:
+% M = 74
+% Beta = 4.5335
+% Wn = 0.5 - Frequencia de corte (0.5*pi)
+% ftype = low (Low Pass Filter)
+[M, Wn, Beta, ftype] = kaiserord(fcuts, mags, devs, fsamp);
 
-[n,Wn,beta,ftype] = kaiserord(fcuts,mags,devs,fsamp);
-
-% Forma direta
-hh = fir1(n,Wn,ftype,kaiser(n+1,beta),'scale');
+% Janela de Kaiser na forma direta
+hh = fir1(M, Wn, ftype, kaiser(M+1, Beta), 'scale');
 
 % Forma cascata (seções de segunda ordem)
-[z,p,k] = tf2zpk(hh,1);
-sos = zp2sos(z,p,k);
+% Discrete-time transfer function to zero-pole conversion. => Converte a
+% funcao filtro para uma matriz de zeros/polos/ganhos
+[z, p, k] = tf2zpk(hh, 1);
+% Zero-pole-gain to second-order sections model conversion
+% Converte a matriz de zeros/polos/ganhos para um modelo de secoes de
+% segunda ordem
+sos = zp2sos(z, p, k);
 
-% filtro na forma direta
+% Filtro ideal
+% na forma direta
 fvtool(hh);
-% filtro em cascata em seções de segunda ordem
+% em cascata em seções de segunda ordem
 fvtool(sos);
 
 % arredondando para 5 casas decimais
@@ -67,16 +84,9 @@ for i = 1:length(hh)
 end
 fvtool(sys1);
 
-% filtro com z^-1 => z^2; rejeita faixa
+% filtro com z^-1 => -z^-2; passa faixa
 sys2 = zeros(1);
 for i = 1:length(hh)
-    sys2((i*2)) = hh(i);
+    sys2((i*2)-1) = hh(i)*(-1)^(i-1);
 end
 fvtool(sys2);
-
-% filtro com z^-1 => -z^-2; passa faixa
-sys3 = zeros(1);
-for i = 1:length(hh)
-    sys3((i*2)-1) = hh(i)*(-1)^(i-1);
-end
-fvtool(sys3);
